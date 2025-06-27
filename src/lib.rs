2d06fe955cb7a0c6c5b1b81a6e800fb475dbd48f
Derukugi <159861671+Derukugis@users.noreply.github.com>
2025-06-27 03:25:11 +0000

    nevermind it all sucked im redoing from scratch

--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,13 @@ mod tests {
 
     #[test]
     fn test_something() {
-        let value = tan(60);
-        println!("tan(60) = {}", value);
+        let sin: f64 = sin(90);
+        let cos: f64 = cos(0);
+        let tan: f64 = tan(45);
+        
+        println!("sin(90) = {}", sin);
+        println!("cos(0) = {}", cos);
+        println!("tan(60) = {}", tan);
     }
 }
 
-pub fn _sin<T: Into<f64>>(_x: T) -> f64 {
-    0.0
-}
-
-pub fn _cos<T: Into<f64>>(_x: T) -> f64 {
-    0.0
-}
-
-pub fn tan<T: Into<f64>>(x: T) -> f64 {
-    let y: f64 = x.into() * (PI / 180.0);
-    let siny: f64 = y - (y.powi(3) / 6.0) + (y.powi(5) / 120.0) - (y.powi(7) / 5040.0);
-    let cosy: f64 = 1.0 - (y.powi(2) / 2.0) + (y.powi(4) / 24.0) - (y.powi(6) / 720.0);
-    let tanx: f64 =  siny / cosy;
-    tanx
-}
