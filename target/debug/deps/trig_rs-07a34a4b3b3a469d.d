bd18203a125a5617f191e47899dce24a8b21a4bd
Derukugi <159861671+Derukugis@users.noreply.github.com>
2025-06-27 02:46:15 +0000

    remove target dir from git

--- target/debug/deps/trig_rs-07a34a4b3b3a469d.d
+++
@@ -1,7 +0,0 @@
-/workspaces/Exact-trigonometric-ratios/target/debug/deps/libtrig_rs-07a34a4b3b3a469d.rmeta: src/lib.rs
-
-/workspaces/Exact-trigonometric-ratios/target/debug/deps/libtrig_rs-07a34a4b3b3a469d.rlib: src/lib.rs
-
-/workspaces/Exact-trigonometric-ratios/target/debug/deps/trig_rs-07a34a4b3b3a469d.d: src/lib.rs
-
-src/lib.rs:
